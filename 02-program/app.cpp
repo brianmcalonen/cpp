4d5dc670077b8ddfefb48fb71763af8f6d6ecc32
Brian McAlonen <bmcalonen@fb.com>
2020-09-12 11:04:49 -0500

    added menu and user prompts

--- 02-program/app.cpp
+++ 02-program/app.cpp
@@ -7,20 +7,35 @@ int main()
 
     char choice;
     double balance;
+    double transaction_amount;
 
     // set the numeric output formatting
     cout << fixed << showpoint << setprecision(2);
 
-    // display the menu
+    // display the progam's title
     cout << "----------------------------" << endl;
     cout << "Checkbook Balancing Program" << endl;
     cout << "----------------------------" << endl;
 
     // prompt the user for their initial balance
-    cout << "\nEnter the beginning balance: ";
+    cout << "Enter the beginning balance: ";
     cin >> balance;
 
-    cout << "\nInitial balance: " << balance << endl;
+    // display the menu
+    cout << "Commands:" << endl;
+    cout << "C - process a check" << endl;
+    cout << "D - process a deposit" << endl;
+    cout << "E - end the program" << endl;
+
+    // Enter transaction type: C
+    cout << "Enter transaction type: ";
+    cin >> choice;
+
+    // Enter transaction amount: 50.25
+    cout << "Enter transaction amount: ";
+    cin >> transaction_amount;
+
+    cout << choice << " " << transaction_amount << endl;
 
     return 0;
 }
