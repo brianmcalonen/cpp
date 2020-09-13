d6a1b5be539edb6a746bfb845276ea3063a7fc90
Brian McAlonen <bmcalonen@fb.com>
2020-09-13 10:23:38 -0500

    processing a check

--- 02-program/app.cpp
+++ 02-program/app.cpp
@@ -4,54 +4,59 @@ using namespace std;
 
 int main()
 {
+    const double SERVICE_CHARGE = 0.35;
 
-    char choice;
+    char menu_choice;
     double balance;
     double transaction_amount;
+    double total_charges;
+    
 
     // set the numeric output formatting
     cout << fixed << showpoint << setprecision(2);
 
     // display the progam's title
     cout << "\nCheckbook Balancing Program" << endl;
-    cout << "----------------------------" << endl;
+    cout << "----------------------------\n" << endl;
 
     // prompt the user for their initial balance
     cout << "Enter the beginning balance: ";
     cin >> balance;
 
     // display the menu
-    cout << "Commands:" << endl;
+    cout << "\nCommands:" << endl;
     cout << "C - process a check" << endl;
     cout << "D - process a deposit" << endl;
-    cout << "E - end the program" << endl;
+    cout << "E - end the program\n" << endl;
 
-    // Enter transaction type: C
+    // prompt the user for their transaction menu_choice
     cout << "Enter transaction type: ";
-    cin >> choice;
+    cin >> menu_choice;
 
-    // Enter transaction amount: 50.25
+    // prompt the user for the transaction amount
     cout << "Enter transaction amount: ";
     cin >> transaction_amount;
 
-    // switch statement based on user's choice 
-    switch (choice) 
+    // switch statement based on user's menu_choice 
+    switch (menu_choice) 
     {
         case 'c':
-            cout << choice;
+        case 'C':
+            // Subtract transaction amount from balance
+            balance -= transaction_amount;
 
-            //Enter transaction type: C
-            // Enter transaction amount: 250
-            // Processing check for $250.00
-            // Balance: $599.75
-            // Service charge: $0.35 for a check
-            // Total service charges: $.70
+            // Add service fee to total charges
+            total_charges += SERVICE_CHARGE;
 
+            cout << "\nProcessing check for " << transaction_amount << endl;
+            cout << "Balance: $" << balance << endl;
+            cout << "Service charge: $0.35 for a check." << endl;
+            cout << "Total service charges: $" << total_charges << " for a check.\n" << endl;
             break;
         
         case 'd':
         case 'D':
-            cout << choice;
+            cout << menu_choice;
 
             // Enter transaction type: D
             // Enter transaction amount: 200
@@ -63,16 +68,16 @@ int main()
 
         case 'e':
         case 'E':
-            cout << choice;
+            cout << menu_choice;
 
             // Enter transaction type: E
             // Processing end of month
             // Final balance: $799.05
-            
+
             break;
 
         default:
-            cout << "Please enter a valid choice" << endl;
+            cout << "Please enter a valid menu_choice" << endl;
 
     }
 
