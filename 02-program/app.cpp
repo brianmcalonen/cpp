810cdc88a6e5c239e0800ddc3ebf4a5a199d73ed
Brian McAlonen <bmcalonen@fb.com>
2020-09-13 20:43:54 -0500

    finished app

--- 02-program/app.cpp
+++ 02-program/app.cpp
@@ -7,14 +7,10 @@ int main()
     const double SERVICE_CHARGE = 0.35;
 
     char menu_choice;
-    double balance;
+    double total_balance;
     double transaction_amount;
     double total_charges;
     bool program_running = true;
-    
-
-    // set the numeric output formatting
-    cout << fixed << showpoint << setprecision(2);
 
     // display the progam's title
     cout << "\nCheckbook Balancing Program" << endl;
@@ -22,7 +18,10 @@ int main()
 
     // prompt the user for their initial balance
     cout << "Enter the beginning balance: ";
-    cin >> balance;
+    cin >> total_balance;
+
+    // set the numeric output formatting
+    cout << fixed << showpoint << setprecision(2);
 
     while(program_running) {
 
@@ -32,11 +31,11 @@ int main()
         cout << "D - process a deposit" << endl;
         cout << "E - end the program\n" << endl;
 
-        // prompt the user for their transaction menu_choice
+        // prompt the user for their menu choice
         cout << "Enter transaction type: ";
         cin >> menu_choice;
 
-        // switch statement based on user's menu_choice 
+        // switch statement based on user's choice 
         switch (menu_choice) 
         {
             case 'c':
@@ -44,16 +43,24 @@ int main()
                 // prompt the user for the transaction amount
                 cout << "Enter transaction amount: ";
                 cin >> transaction_amount;
+                
+                // validate user input
+                if (transaction_amount <= 0)
+                {
+                    cout << "Please enter a valid number" << endl;
+                    cout << "Enter transaction amount: ";
+                    cin >> transaction_amount;
+                }
 
-                // Subtract transaction amount from balance
-                balance -= transaction_amount;
+                // Subtract transaction amount from thew balance
+                total_balance -= transaction_amount;
 
                 // Add service fee to total charges
                 total_charges += SERVICE_CHARGE;
 
                 cout << "\nProcessing check for $" << transaction_amount << endl;
-                cout << "Balance: $" << balance << endl;
-                cout << "Service charge: $0.35 for a check." << endl;
+                cout << "Balance: $" << total_balance << endl;
+                cout << "Service charge: $0.35 for a check" << endl;
                 cout << "Total service charges: $" << total_charges << "\n" << endl;
                 
                 break;
@@ -64,21 +71,29 @@ int main()
                 cout << "Enter transaction amount: ";
                 cin >> transaction_amount;
 
-                // Add transaction amount to balance
-                balance += transaction_amount;
+                // validate user input
+                if (transaction_amount <= 0)
+                {
+                    cout << "Please enter a valid number" << endl;
+                    cout << "Enter transaction amount: ";
+                    cin >> transaction_amount;
+                }
+
+                // Add transaction amount to total balance
+                total_balance += transaction_amount;
 
                 cout << "\nProcessing deposit for $" << transaction_amount << endl;
-                cout << "Balance: $" << balance << endl;
+                cout << "Balance: $" << total_balance << endl;
                 cout << "Total service charges: $" << total_charges << "\n" << endl;
 
                 break;
 
             case 'e':
             case 'E':
-                // Subtract total service charges from the balance
-                balance -= total_charges;
-                cout << "Processing end of month..." << endl;
-                cout << "Final balance: $" << balance << "\n" << endl;
+                // Subtract total service charges from the total balance
+                total_balance -= total_charges;
+                cout << "Processing end of month:" << endl;
+                cout << "Final balance: $" << total_balance << "\n" << endl;
 
                 program_running = false;
 
