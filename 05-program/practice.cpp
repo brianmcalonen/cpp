05a692a05fc7a33d941a50bc4608c193412c819b
Brian McAlonen <bmcalonen@fb.com>
2020-10-07 11:01:03 -0500

    passing arrays and sizeof

--- 05-program/practice.cpp
+++ 05-program/practice.cpp
@@ -2,7 +2,27 @@
 #include <string>
 using namespace std;
 
-struct User
+// Passing Arrays to Functions and sizeof Operator
+
+int main()
+{
+    int guesses[10] = {12, 43, 23, 43, 23};
+
+    int num_elements = 5;
+    int size = sizeof(guesses) / sizeof(int);
+
+    cout << size << endl;
+
+    for(int i = 0; i < num_elements; i++)
+    {
+        cout << guesses[i] << "\t";
+    }
+    
+    return 0;
+}
+
+// Struct Basics
+/* struct User
 {
     string first_name;
     string last_name;
@@ -27,4 +47,4 @@ int main()
     cout << "I have " << me.get_status() << " status!" << endl;
 
     return 0;
-}
+} */
