3e44b8fc11917f96b5b127a920f4f067b2276b39
Brian McAlonen <bmcalonen@fb.com>
2020-10-09 21:23:39 -0500

    exam notes

--- practice_exams/01-questions.cpp
+++ practice_exams/01-questions.cpp
@@ -1,100 +1,32 @@
 #include <iostream>
 #include <iomanip> 
 
-///////////////////////////////////////////////////////////////////////////////
-// Question 1
+Chapter 1 - Intro to Programming
+Computers can do many different jobs because they can be programmed.
+Internally, the CPU consists of the arithmetic logic unit (ALU) and the control unit.
+The two general categories of software are operating systems and application software.
+Since computer’s can’t be programmed in natural human language, algorithms must be written in a programming language.
+High-level languages are close to the level of humans in terms of readability.
+A program’s ability to run on several different types of computer systems is called portability.
+Words or names defined by the programmer are called programmer-defined symbols.
+Punctuation characters or symbols mark the beginning or ending of programming statements.
+A variable is a named storage location.
+The three primary activities of a program are input, processing, and output.
+Output is information a program sends to the outside world.
+Main memory, or RAM, is volatile, which means its contents are erased when power is removed from the computer. Secondary memory, such as a disk, CD, or flash drive, does not lose its contents when power is removed from the computer.
+RAM stands for Random Access Memory.
+ALU stands for Arithmetic and Logic Unit.
 
-// Memory locations that can hold data are called
-// 	[]	operators.
-// 	[x]	variables.
-// 	[]	syntax.
-// 	[]	operands.
-// 	[]	None of these
-
-
-
-///////////////////////////////////////////////////////////////////////////////
-// Question 2
-
-// Characters or symbols that perform operations on one or more operands are
-// 	[]	syntax.
-// 	[]	op codes.
-// 	[x]	operators.
-// 	[]	program ops.
-// 	[]	None of these
-
-
-
-///////////////////////////////////////////////////////////////////////////////
-// Question 3
-
-// Write a function call for a function named distance. The function should 
-// return a double and have two floating-point arguments for rate and time.
-
-double distance(5.0, 9.9);
-
-///////////////////////////////////////////////////////////////////////////////
-// Question 4
-
-// Write a C++ loop that validates user input for a number between 1 through 
-// 100 inclusive.
-// Proper indentation is required. Use the space bar (4 spaces for one tab). 
-// Do NOT use the tab key inside the text box.
-
-double number;
-cout << "Enter a number: " << endl;
-cin >> number;
-
-while (number < 1 || number > 100)
-{
-    cout << "Your number must be between 1 - 100 inclusive." << endl;
-    cout << "Enter a number: " << endl;
-    cin >> number;
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// Question 5
-
-// Write a function prototype for a function named distance. 
-// The function should return a double and have two double parameters: 
-// rate and time
-
-double distance(double rate, double time);
-
-///////////////////////////////////////////////////////////////////////////////
-// Question 6
-
-// Write the prototype for a void function named showFees that has 2 parameters, 
-// a string value and a floating-point value.
-
-void showFees(string, float);
+Chapter 2 - Intro to C++
+Every complete C++ statement ends with a semicolon.
+Every C++ program must have a function named main.
+A group of statements, such as the body of a function, must be enclosed in braces {}.
+978.65 X 10^12 would be written in E notation as 9.7865E14.
+A variable’s scope is the part of the program that has access to the variable.
+The modulus operator computes the remainder of doing integer division.
+Comments are notes of explanation that document lines or sections of a program.
+Single line comment: //		Multi-line comment: /*   */
+Programming style refers to the way a programmer uses identifiers, spaces, tabs, blank lines, and punctuation characters to visually arrange a program’s source code. These are some, but not all, of the elements of programming style.
+int main() { return 0; }
 
 
-///////////////////////////////////////////////////////////////////////////////
-// Question 7
-
-// Write a function named distance that accepts two floating-point arguments 
-// for rate and time. The function should return the distance traveled given 
-// a rate of travel and the time traveled.
-
-double distance(float rate, float time)
-{
-    return rate * time;
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// Question 8
-
-// Write the stub for a value-returning function named ticketRate that has 1 
-// parameter named category (a character) and returns the cost of a ticket 
-// (a floating-point).
-
-
-
-///////////////////////////////////////////////////////////////////////////////
-// Question 9
-
-// Write a function header for a function named distance. The function should 
-// return a double and have two double parameters: rate and time.
-
-double distance(double rate, double time)
