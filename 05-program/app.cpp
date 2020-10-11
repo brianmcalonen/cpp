68686c1f1112233d4114370008130f34c646c554
Brian McAlonen <bmcalonen@fb.com>
2020-10-11 12:32:22 -0500

    append to struct array

--- 05-program/app.cpp
+++ 05-program/app.cpp
@@ -6,16 +6,16 @@
 
 // Payroll program for Armadillo Automotive Group
 
-// Your program should first input the employee master information 
-// into an array of structs. Then use a separate loop to do the 
-// payroll processing for each employee (input the employee's hours 
+// Your program should first input the employee master information
+// into an array of structs. Then use a separate loop to do the
+// payroll processing for each employee (input the employee's hours
 // worked and calculate their pay
 
 //******************************************************************
 
 #include <iostream> // input/output declarations
 #include <iomanip>  // i/o manupulator declarations
-#include <string>   // 
+#include <string>   //
 using namespace std;
 
 // The employee master information consists of the following data:
@@ -34,25 +34,58 @@ struct Employee
 
 int main()
 {
+    const int NUM_EMPLOYEES = 4;
+
     // array to hold 4 structs
-    Employee employee_array[3];
+    Employee employee_array[NUM_EMPLOYEES];
 
     int id;
     string name;
+    double rate;
+    int work_type;
 
-    cout << "Enter information for employee 1" << endl;
+    for (int i = 0; i < NUM_EMPLOYEES; i++)
+    {
+        cout << "Enter information for employee " << (i + 1) << endl;
+        cout << "Employee ID: ";
+        cin >> id;
+        // clear buffer before taking new line
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+        cout << "Employee Name: ";
+        getline(cin, name);
+        cout << "Pay Rate: ";
+        cin >> rate;
+        cout << ("Type: ");
+        cin >> work_type;
 
-    cout << "Employee ID: ";
-    cin >> id;
+        employee_array[i].id_number = id;
+        employee_array[i].full_name = name;
+        employee_array[i].pay_rate = rate;
+        employee_array[i].emp_type = work_type;
 
-    // clear buffer before taking new line
-    cin.ignore(); 
+        // clear buffer before taking new line
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+    }
 
-    cout << "Employee Name: ";
-    getline(cin, name);
+    // set the numeric output formatting
+    cout << fixed << showpoint << setprecision(2);
 
-    cout << id << endl;
-    cout << name << endl;
+    cout << employee_array[0].id_number << endl;
+    cout << employee_array[0].full_name << endl;
+    cout << employee_array[0].pay_rate << endl;
+    cout << employee_array[0].emp_type << endl;
+    cout << employee_array[1].id_number << endl;
+    cout << employee_array[1].full_name << endl;
+    cout << employee_array[1].pay_rate << endl;
+    cout << employee_array[1].emp_type << endl;
+    cout << employee_array[2].id_number << endl;
+    cout << employee_array[2].full_name << endl;
+    cout << employee_array[2].pay_rate << endl;
+    cout << employee_array[2].emp_type << endl;
+    cout << employee_array[3].id_number << endl;
+    cout << employee_array[3].full_name << endl;
+    cout << employee_array[3].pay_rate << endl;
+    cout << employee_array[3].emp_type << endl;
 
     return 0;
 }
