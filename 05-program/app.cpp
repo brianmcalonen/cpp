397441fca11318051ef1fa6174fe8c5f6a83cd9f
Brian McAlonen <bmcalonen@fb.com>
2020-10-11 11:39:08 -0500

    getline function

--- 05-program/app.cpp
+++ 05-program/app.cpp
@@ -3,12 +3,13 @@
 // Programmer: Brian McAlonen
 // Completed : 10/11/2020
 // Status    : Complete
-//
+
+// Payroll program for Armadillo Automotive Group
+
 // Your program should first input the employee master information 
 // into an array of structs. Then use a separate loop to do the 
 // payroll processing for each employee (input the employee's hours 
 // worked and calculate their pay
-
 
 //******************************************************************
 
@@ -33,7 +34,25 @@ struct Employee
 
 int main()
 {
-    Employee me;
+    // array to hold 4 structs
+    Employee employee_array[3];
+
+    int id;
+    string name;
+
+    cout << "Enter information for employee 1" << endl;
+
+    cout << "Employee ID: ";
+    cin >> id;
+
+    // clear buffer before taking new line
+    cin.ignore(); 
+
+    cout << "Employee Name: ";
+    getline(cin, name);
+
+    cout << id << endl;
+    cout << name << endl;
 
     return 0;
 }
