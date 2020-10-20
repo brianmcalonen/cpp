6d295534bb7d2c1b1dd171e95142f45a38f4b911
Brian McAlonen <bmcalonen@fb.com>
2020-10-20 15:34:02 -0500

    inputFood function

--- 06-program/app.cpp
+++ 06-program/app.cpp
@@ -4,22 +4,44 @@
 // Completed : 10/18/2020
 // Status    : Complete
 
-// 
+//
 //******************************************************************
 
 #include <iostream> // input/output declarations
 #include <iomanip>  // i/o manupulator declarations
+#include <string>
 using namespace std;
+
+// Function prototypes
+void inputFood(int, int, string[], double[]);
+void printTable();
+double averageFood();
+double leastFood();
+double mostFood();
 
 int main()
 {
+    const int NUM_ROWS = 3;
+    const int NUM_COLS = 7;
+    string header[] = {"Monkey", "Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"};
+    double monkey_array[NUM_ROWS][NUM_COLS];
 
-    // Function prototypes
-    void inputFood();
-    void printTable();
-    double averageFood();
-    double leastFood();
-    double mostFood();
+    // Function calls
+    inputFood(NUM_ROWS, NUM_COLS, header, monkey_array);
 
     return 0;
+}
+
+void inputFood(int NUM_ROWS, int NUM_COLS, string header[], double monkey_array[])
+{
+    for (int i = 0; i < NUM_ROWS; i++)
+    {
+        for (int j = 0; j < NUM_COLS; j++)
+        {
+            cout << "Enter pounds of food eaten by monkey " << (i + 1) << "on " << header[i + 1] << ": " << endl;
+            cin >> monkey_array[i][j];
+        }
+    }
+
+    cout << monkey_array << endl;
 }
