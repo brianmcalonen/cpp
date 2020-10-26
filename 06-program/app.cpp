7e36680bcc4be712651ef33418643c0637319bb7
Brian McAlonen <bmcalonen@fb.com>
2020-10-26 12:33:57 -0500

    print table for food array

--- 06-program/app.cpp
+++ 06-program/app.cpp
@@ -35,6 +35,23 @@ void inputFood(string headerArr[], double foodArr[NUM_ROWS][NUM_COLS])
 
 void printTable(string headerArr[], double foodArr[NUM_ROWS][NUM_COLS])
 {
+
+    // print header and format
+    cout << setw(7) << headerArr[0] << setw(7) << headerArr[1] << setw(7) << headerArr[2] << setw(7)
+    << headerArr[3] << setw(7) << headerArr[4] << setw(7) << headerArr[5] << setw(7) << headerArr[6]
+    << setw(7) << headerArr[7] << endl;
+
+    for (int i = 0; i < NUM_ROWS; i++)
+    {
+        for (int j = 0; j < NUM_COLS; j++)
+        {
+            cout << setw(7) << foodArr[i][j] << setw(7) << foodArr[i][j] << setw(7) << foodArr[i][j] << setw(7)
+    << foodArr[i][j] << setw(7) << foodArr[i][j] << setw(7) << foodArr[i][j] << setw(7) << foodArr[i][j]
+    << setw(7) << foodArr[i][j] << endl;
+
+        }
+    }
+
     cout << "print table" << endl;
 }
 
@@ -42,6 +59,9 @@ int main()
 {
     string header[] = {"Monkey", "Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"};
     double monkey_array[NUM_ROWS][NUM_COLS];
+
+    // set precision of output
+    cout << fixed << setprecision(2);
 
     // Function calls
     // inputFood(header, monkey_array);
