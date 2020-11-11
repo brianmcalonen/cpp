6794bd25da5c5c764c38a8d9157217b6c1d76376
Brian McAlonen <bmcalonen@fb.com>
2020-11-11 13:09:50 -0600

    userInput function

--- 07-program/app.cpp
+++ 07-program/app.cpp
@@ -11,3 +11,35 @@
 #include <iomanip>  // i/o manupulator declarations
 #include <string>
 using namespace std;
+
+// Function prototypes
+void userInput(int, int, int, int);
+
+
+int main()
+{
+    int weight,
+        side1,
+        side2,
+        side3;
+
+    // Function calls
+    userInput(weight, side1, side2, side3);
+
+
+    return 0;
+}
+
+// userInput Fuction definition
+void userInput(int weight, int side1, int side2, int side3)
+{
+
+    cout << "enter your input: " << endl;
+    cin >> weight >> side1 >> side2 >> side3;
+
+    cout << weight << endl;
+    cout << side1 << endl;
+    cout << side2 << endl;
+    cout << side3 << endl;
+
+}
