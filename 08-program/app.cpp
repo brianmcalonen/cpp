cafe187211228272945f9f96eff5d6ea09afcf13
Brian McAlonen <bmcalonen@fb.com>
2020-11-25 10:00:02 -0600

    boilerplate and psuedocode

---
+++ 08-program/app.cpp
@@ -0,0 +1,44 @@
+//******************************************************************
+// Assignment 8 - Test Scores
+// Programmer: Brian McAlonen
+// Completed : 11/29/2020
+// Status    : Complete
+
+//
+//******************************************************************
+
+#include <iostream> // input/output declarations
+#include <iomanip>  // i/o manupulator declarations
+using namespace std;
+
+// Function prototypes
+void whatever();
+
+// main Function definition
+int main()
+{
+    int students = 0;
+
+    cout << endl;
+    cout << "Enter the number of students to be processed: " << endl;
+    cin >> students;
+
+    // check students is a valid number
+
+    // use the new operator to dynamically allocate an array of that size
+
+    // Function calls
+
+    // Call a function to input and validate the student grades and store them in the array.
+
+    // Call a function to sort the array in ascending (increasing) order.
+
+    // Call a value-returning function that returns the floating-point average of the grades.
+        // Use only pointer notation in your function to calculate the average.
+        // you must declare the array parameter using pointer notation instead of array notation.
+        // inside the function you must use pointer notation instead of array notation to access the array elements
+
+    // Call a function that displays a neat table of student grades in sorted order.
+
+    return 0;
+}
