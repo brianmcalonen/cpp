4dc8a73566549ca38dbc8145a1454f812652a4aa
Brian McAlonen <bmcalonen@fb.com>
2020-11-29 16:33:34 -0600

    finished code

--- 08-program/app.cpp
+++ 08-program/app.cpp
@@ -4,45 +4,50 @@
 // Completed : 11/29/2020
 // Status    : Complete
 
-//
+// Program to calculate the average test score of a group of
+// students using pointers and pointer notation.
 //******************************************************************
 
 #include <iostream> // input/output declarations
 #include <iomanip>  // i/o manupulator declarations
+#include <algorithm>
 using namespace std;
 
 // Function prototypes
 void inputGrades(int *, int);
+void sortGrades(int *, int);
+double averageGrades(int *, int);
+void printGrades(int *, int);
 
 // main Function definition
 int main()
 {
+    // set students variable equal to 0
     int students = 0;
+    // set pointer to an int equal to null
     int *grade_ptr = nullptr;
 
-    cout << endl;
-    cout << "Enter the number of students to be processed: ";
+    // prompt the user for the number of students
+    cout << "\nEnter the number of students to be processed: ";
+    // set the students variable equal to the user's input
     cin >> students;
 
-
-    // check students is a valid number
-    if (students < 0)
+    // check if number students is a valid number
+    if (students < 1)
     {
-        cout << "Please enter a valid number of students to be processed: ";
-        cin >> students;
+        // print an error message
+        cout << "\nPlease enter a valid number of students to be processed." << endl;
+        // restart the main program
+        main();
     }
 
     // use the new operator to dynamically allocate an array of that size
     grade_ptr = new int[students];
 
-    // Function calls
+    // function calls
     inputGrades(grade_ptr, students);
-
-    // print grade_ptr array
-    for(int i = 0; i < students; i++)
-    {
-        cout << grade_ptr[i] << endl;
-    }
+    sortGrades(grade_ptr, students);
+    printGrades(grade_ptr, students);
 
     // free allocated storage
     delete [] grade_ptr;
@@ -53,33 +58,70 @@ int main()
 // function to input and validate the student grades and store them in the array
 void inputGrades(int *grade_ptr, int students)
 {
+    // loop for each student
     for(int i = 0; i < students; i++)
     {
+        // prompt user for grade for specific student
         cout << "\nEnter Grade for Student #" << i + 1 << ": ";
-        
+        // put the grade into the grade array via the pointer
         cin >> grade_ptr[i];
 
+        // if the grade entered by the user is invalid
         if (grade_ptr[i] < 0 || grade_ptr[i] > 105)
         {
-            cout << "Error - Please enter a valid score";
+            // print an error message
+            cout << "\nError - Please enter a valid score" << endl;
+            // decrement the loops index
             i--;
         }
     }
 
 }
 
-    // Function to sort the array in ascending (increasing) order.
+// Function to sort the array in ascending (increasing) order.
+void sortGrades(int *grade_ptr, int students)
+{
+    // use the sort method to sort grades in ascending order
+    sort(grade_ptr, grade_ptr + students);
+}
 
-    // Value-returning function that returns the floating-point average of the grades.
-        // Use only pointer notation in your function to calculate the average.
-        // you must declare the array parameter using pointer notation instead of array notation.
-        // inside the function you must use pointer notation instead of array notation to access the array elements
+// Value-returning function that returns the floating-point average of the grades.
+double averageGrades(int *grade_ptr, int students)
+{
+    // variable to hold the total scores from all grades
+    double total = 0.0;
 
-        // int scores[] {100, 95, 87, -1};
-        // int *score_ptr {scores};
-        // score_ptr = scores;
-        // while(*score_ptr != -1)
-        //     cout << *score_ptr++ << endl;
+    // for each students grade
+    for(int i = 0; i < students; i++)
+    {
+        // use pointer notation to add the student's grade to the total
+        total += *(grade_ptr + i);
+    }
 
+    // return the average of all students test scores
+    return total / students;
+}
 
-    // Void function that displays a neat table of student grades in sorted order.
+// Void function that displays a neat table of student grades in sorted order.
+void printGrades(int *grade_ptr, int students)
+{
+    // set the numeric output formatting
+    cout << fixed << showpoint << setprecision(2);
+
+    // print the output header
+    cout << endl;
+    cout << setw(9) << left << " " << setw(6) << right << "Grades" << endl;
+    cout << setw(9) << left << " " << setw(6) << right << "------" << endl;
+
+    // loop for each student
+    for(int i = 0; i < students; i++)
+    {
+        // print each students test score
+        cout << setw(9) << left << " " << setw(6) << right << grade_ptr[i] << endl;
+    }
+
+    // print the average test score of all students
+    cout << setw(9) << left << " " << setw(6) << right << "------" << endl;
+    cout << setw(9) << left << "Average" << setw(6) << right << averageGrades(grade_ptr, students) << endl;
+    cout << endl;
+}
